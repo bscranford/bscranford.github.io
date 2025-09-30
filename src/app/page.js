8d1ab3475ccf532ae7e6ba1fc84fac2afbd46b2b
bscranford <bscranford@gmail.com>
2025-09-30 19:46:00 -0400

    update home page

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <main>
             <header id="hero">
-                <h1>Brad<br />Cran<br />ford</h1>
+                <h1>Brad Cranford</h1>
                 <Image src={caret.src} width={50} height={50} alt="Downward facing caret" />
             </header>
             <section className="content-wrapper">
